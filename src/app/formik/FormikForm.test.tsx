7742ff1e3ced96a8f7f7cb3632e33b7798cbffa1
taro-28 <tarotaro0208@gmail.com>
2023-09-24 09:35:06 +0900

    split test for create and update

--- src/app/formik/FormikForm.test.tsx
+++ src/app/formik/FormikForm.test.tsx
@@ -1,39 +1,45 @@
-import React from 'react'
-import FormikPage from './page'
+import React, { ComponentPropsWithoutRef } from 'react'
 import { test, expect, describe } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { typedEntries } from '@/funtions'
 import { inputTypeToRoleMap } from '../consts/inputTypeToRoleMap'
-import { HTMLInputType } from '@/types'
 import { FormikForm } from './FormikForm'
 
-const initialValues: Partial<Record<HTMLInputType, any>> = {
-  text: '',
-  number: null,
-  email: '',
-  datetime: '',
-  tel: '',
-  url: '',
-  search: '',
-  range: '0',
-  checkbox: false,
-  // cannot be tested
-  // password: '',
-  // date: '',
-  // time: '',
-  // month: '',
-  // week: '',
-  // color: '',
-  // radio: '',
-  // file: '',
-  // image: '',
-  // 'datetime-local': '',
-}
-
-// split test for create and update
 describe('FormikForm', () => {
-  test(`should render input`, () => {
+  test.each<{
+    name: string
+    initialValues: ComponentPropsWithoutRef<typeof FormikForm>['initialValues']
+  }>([
+    {
+      name: 'should render input empty initial values',
+      initialValues: {
+        text: '',
+        number: null,
+        email: '',
+        datetime: '',
+        tel: '',
+        url: '',
+        search: '',
+        range: '0',
+        checkbox: false,
+      },
+    },
+    {
+      name: 'should render input with initial values',
+      initialValues: {
+        text: 'test',
+        number: 1,
+        email: 'test@example.com',
+        datetime: '2021-01-01T00:00:00',
+        tel: '0123456789',
+        url: 'https://example.com',
+        search: 'test search',
+        range: '1',
+        checkbox: true,
+      },
+    },
+  ])('$name', ({ initialValues }) => {
     render(<FormikForm initialValues={initialValues} onSubmit={() => {}} />)
     typedEntries(initialValues).forEach(async ([name, initialValue]) => {
       const role = inputTypeToRoleMap.get(name)!
@@ -41,7 +47,11 @@ describe('FormikForm', () => {
 
       switch (role) {
         case 'checkbox':
-          expect(await findTarget()).not.toBeChecked()
+          if (!initialValue) {
+            expect(await findTarget()).not.toBeChecked()
+            break
+          }
+          expect(await findTarget()).toBeChecked()
           break
         default:
           expect(await findTarget()).toHaveValue(initialValue)
