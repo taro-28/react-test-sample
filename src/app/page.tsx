26a36592f50bb2a9a28876a540c6fe187705778f
taro-28 <tarotaro0208@gmail.com>
2023-09-30 18:51:38 +0900

    update app name to react test sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <Link className='text-blue-500 hover:text-blue-300' href='/formik'>
-      Formik
+      Formik Form
     </Link>
   )
 }
